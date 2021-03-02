ce5858032bc3bb233ac06595106decc4d109ba7d
Nicholas Dudash <nddudash@gmail.com>
2021-03-02 12:07:08 -0700

    Should be enough for all the rubric points. I'll play around with it a little more.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Card, Button } from "react-bootstrap";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [user, setUser] = useState({});
+  const [active, setActive] = useState(false);
+
+  function handleToggle(event) {
+    fetch("https://api.github.com/users/nddudash", { method: "GET" })
+      .then((response) => response.json())
+      .then((userObject) => setUser(userObject))
+      .then(setActive(!active));
+  }
+
+  let userCard;
+  active && user
+    ? (userCard = (
+        <Card className="userCard" style={{ width: `33%` }}>
+          <Card.Img
+            className="userCardAvatar"
+            src={user.avatar_url}
+            alt="user_avatar"
+          />
+          <Card.Title className="userCardTitle">{user.login}</Card.Title>
+          <Card.Footer className="userCardFooter">
+            <div className="userCardRepoCount">
+              Repositories: <a href={user.repos_url}>{user.public_repos}</a>
+            </div>
+            <div className="userCardFollowerCount">
+              Followers: {user.followers}
+            </div>
+          </Card.Footer>
+        </Card>
+      ))
+    : (userCard = null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className="toggleButton" onClick={handleToggle}>
+        Toggle
+      </Button>
+      {userCard}
     </div>
   );
 }
